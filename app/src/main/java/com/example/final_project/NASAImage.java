29c3d553da7ef17b6308085f6c5a1095e6ff6e79
sarbs <sarbsarbsagar143@gmail.com>
2020-04-07 09:51:42 -0400

    cmt2

--- app/src/main/java/com/example/final_project/NASAImage.java
+++ app/src/main/java/com/example/final_project/NASAImage.java
@@ -1,7 +1,15 @@
 package com.example.final_project;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
+import com.example.final_project.ui.main.PhotoDatabase;
+import com.example.final_project.ui.main.SavedImagesFragment;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
 import androidx.viewpager.widget.ViewPager;
@@ -23,6 +31,21 @@ public class NASAImage extends AppCompatActivity {
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+                builder.setTitle("Instructions");
+                builder.setMessage("Search:\n1) Choose a date in Present/Past\n2)Click on Search\n3)Save Image by Clicking 'SAVE IMAGE' Button.\n\n" +
+                        "Saved Images:\n1) Swipe to left to access saved Image List\n2) Click to View Image\n" +
+                        "3) Long Press item to Delete");
+
+                AlertDialog dialog = builder.create();
+                // Display the alert dialog on interface
+                dialog.show();
+            }
+        });
 
 
     }
